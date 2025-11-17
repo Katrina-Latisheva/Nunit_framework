9b05842d9ce13d2da16e27c58b2dc34d35146ff2
Katrina-Latisheva <katrinalatisheva@gmail.com>
2025-11-17 21:18:45 +0200

    Created project sempl

---
+++ NunitTests/NunitTests/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace NunitTests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
